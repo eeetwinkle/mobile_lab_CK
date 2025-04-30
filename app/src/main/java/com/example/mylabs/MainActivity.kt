19b0fa44b29120864860377393c265b84582e185
ermolaevaalenaandreevna <alyonaerm13@gmail.com>
2025-04-30 20:38:48 +1000

    its lab2

--- app/src/main/java/com/example/mylabs/MainActivity.kt
+++ app/src/main/java/com/example/mylabs/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.mylabs
-
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_welcome_screen)
+
+        findViewById<MaterialButton>(R.id.loginButton).setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
+
+        findViewById<MaterialButton>(R.id.registerButton).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
